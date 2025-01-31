ac437949e43cb3b906679089f98a622a03340766
max <hmwright404@gmail.com>
2025-01-31 14:47:40 -0600

    solution

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,4 @@
+import random
 class GuessingGame():
     """
     Initialized with an integer
@@ -20,6 +21,34 @@ class GuessingGame():
     
     """
     # Write your code here
-
+    def __init__(self, answer=random.randint(1,100)):
+        self.answer = answer
+        self.last_result = None #store result of the last guess
     
-    pass
+    def guess(self, user_guess):
+        if user_guess > self.answer:
+            self.last_result = 'high'
+        elif user_guess == self.answer:
+            self.last_result = 'correct'
+        else:
+            self.last_result = 'low'
+        return self.last_result
+    def solved(self):
+        return self.last_result == 'correct'
+        
+
+game = GuessingGame(random.randint(1,100))
+last_guess  = None
+last_result = None
+
+while game.solved() == False:
+  if last_guess != None: 
+    print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+    print("")
+
+  last_guess  = input("Enter your guess: ")
+  last_guess = int(last_guess)
+  last_result = game.guess(last_guess)
+
+
+print(f"{last_guess} was correct!")
