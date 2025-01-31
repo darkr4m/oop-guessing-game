c41e9b4c58fa16c53fffbef284e585f9d7645f33
max <hmwright404@gmail.com>
2025-01-31 14:01:46 -0600

    psuedocode

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,25 @@
 class GuessingGame():
+    """
+    Initialized with an integer
+    Start game with random number
+    Instance methods
+        #init
+            answer - integer (random number)
+        #guess
+            input:
+                user_guess - integer
+            output:
+                If user_guess > answer = high (string)
+                if user_guess == answer = correct (string)
+                If user_guess < answer = low (string)
+        #solved
+            if most recent user_guess == correct
+                output = True (boolean)
+            if most recent user_guess !== correct
+                output = False (boolean)
+    
+    """
     # Write your code here
+
+    
     pass
